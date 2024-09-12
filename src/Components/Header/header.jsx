4b401d4004cb9ad902652ddb81cca87e490a32ab
Lauren Coates <lspassey@gmail.com>
2024-09-12 16:44:10 -0700

    fixed error in importing for About me page

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -9,7 +9,7 @@ function Header({ setCurrentPage }) {
             </div>
         <h1>Lauren Coates</h1>
         <nav> 
-            <button onClick={() => setCurrentPage('About Me')}>About Me</button>
+            <button onClick={() => setCurrentPage('AboutMe')}>About Me</button>
             <button onClick={() => setCurrentPage('Portfolio')}>Portfolio</button>
             <button onClick={() => setCurrentPage('Contact')}>Contact</button>
             <button onClick={() => setCurrentPage('Resume')}>Resume</button>
